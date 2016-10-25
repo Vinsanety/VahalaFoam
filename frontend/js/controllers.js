1cbb5b62a1cdb10ae5a216ea29d1b0d5d5c6f2fd
vince vahala <vincevahala@gmail.com>
2016-10-24 22:08:00 -0600

    added small nav

--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -3,10 +3,21 @@ angular.module('VahalaFoam.controllers', [])
 .controller('homeController', ['$http', '$scope',
 function($http, $scope) {
   angular.element(document).ready(function() {
-    console.log('Home Route Hit');
+
+    $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'left' // Displays dropdown with edge aligned to the left of button
+      }
+    );
 
     $(".button-collapse").sideNav();
     $('.parallax').parallax();
+
 
   })
 }])
