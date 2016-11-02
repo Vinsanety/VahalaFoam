3025f35abf9b1f6acdaeeaacfdfd33610dda25f2
vince vahala <vincevahala@gmail.com>
2016-11-02 11:52:29 -0600

    made some nav changes

--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -8,7 +8,10 @@ function($http, $scope) {
       inDuration: 300,
       outDuration: 225,
       constrain_width: false, // Does not change width of dropdown to that of the activator
+
+      //Had to play with hover to get it to work live on mobile
       hover: true, // Activate on hover
+
       gutter: 0, // Spacing from edge
       belowOrigin: true, // Displays dropdown below the button
       alignment: 'left' // Displays dropdown with edge aligned to the left of button
